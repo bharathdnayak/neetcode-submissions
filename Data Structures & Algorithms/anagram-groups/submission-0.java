f01242e2c99be21e064ad161a2e70fa172a3dc01
Bharath D Nayak <159288353+bharathdnayak@users.noreply.github.com>
2026-09-11 21:40:40 +0530

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans=new ArrayList<>();
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String s=strs[i];
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(s);
+        }
+         return new ArrayList<>(map.values());
+    }
+}
